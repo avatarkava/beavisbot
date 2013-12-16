8cea787636c45a6208fc138f60ebcbb06a11c100
Kate Wellington <sharedferret@gmail.com>
2013-12-16 09:06:11 -0800

    Fix .mostlamed

--- commands/mostlamed.js
+++ commands/mostlamed.js
@@ -3,7 +3,7 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
-    db.all('SELECT author, title, sum_down FROM (SELECT songid, sum(downvotes) as sum_up FROM PLAYS GROUP BY songid ORDER BY sum(downvotes) DESC LIMIT 3) a INNER JOIN SONGS ON a.songid = SONGS.id', function(error, rows) {
+    db.all('SELECT author, title, sum_down FROM (SELECT songid, sum(downvotes) as sum_down FROM PLAYS GROUP BY songid ORDER BY sum(downvotes) DESC LIMIT 3) a INNER JOIN SONGS ON a.songid = SONGS.id', function(error, rows) {
         if (rows != null) {
             bot.chat('The most lamed songs I\'ve heard: ' + rows.map(function(row) { return row['title'] + ' by ' + row['author'] + ': ' + row['sum_down'] + ' lames.'; }).join(' · '));
         } else {
