8c95e3596f39aca4f4aa461db922d2bbf9a708e1
mburke <michaelvburke@gmail.com>
2014-03-18 19:32:59 -0500

    * Command updates

---
+++ commands/bacon.js
@@ -0,0 +1,7 @@
+exports.names = ['.bacon'];
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function (data) {
+    bot.chat('Beavis & @Bacon_Cheeseburger 4EVA <3');;
+};
--- commands/taco.js
+++ commands/taco.js
@@ -4,7 +4,7 @@ exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function (data) {
     var strings = [
-        'http://static.wixstatic.com/media/a07b92_537fd27edabf4722a7673f5c824c29f6.gif_srz_p_500_500_75_22_0.50_1.20_0.00_gif_srz',
+        'http://static.wixstatic.com/media/a07b92_537fd27edabf4722a7673f5c824c29f6.gif',
         'http://www.thecoast.ca/images/blogimages/2011/02/16/1297864255-taco.gif'
     ];
     var randomIndex = Math.floor(Math.random() * strings.length);
