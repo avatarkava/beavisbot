ef9ef0a4c9a2ebb4621c0f83aae22bf656014bbb
mburke <michaelvburke@gmail.com>
2014-03-20 00:41:34 -0500

    * Initial test of AFK poll for mods

--- commands/afk.js
+++ commands/afk.js
@@ -4,21 +4,30 @@ exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
     if (room.staff[data.fromID] > 1 && config.activeDJTimeoutMins > 0) {
-        maxIdleTime = config.activeDJTimeoutMins * 60;
 
-        // Start at slot 1 (skip the current DJ)
-        for (i = 1; i < room.djs.length; i++) {
-            dj = room.djs[i].user;
+        var maxIdleTime = config.activeDJTimeoutMins * 60;
+        var idleDJs = [];
+
+        for (i = 0; i < room.djs.length; i++) {
+            var dj = room.djs[i].user;
             db.get("SELECT strftime('%s', 'now')-strftime('%s', lastSeen) AS 'secondsSinceLastVisit', lastSeen, username FROM USERS WHERE userid = ?", [dj.id] , function (error, row) {
                 if (row != null) {
                     if(row.secondsSinceLastVisit >= maxIdleTime) {
-                        console.log('[IDLE] ' + row.username + ' visited '+ row.secondsSinceLastVisit + ' seconds ago (' + row.lastSeen + ')');
+                        console.log('[IDLE] ' + row.username + ' visited '+ timeSince(row.lastSeen) + ' ago');
+                        idleDJs.push(row.username + ' (' + timeSince(row.lastSeen) + ')');
                     }
                     else {
-                        console.log('[ACTIVE] ' + row.username + ' visited '+ row.secondsSinceLastVisit + ' seconds ago (' + row.lastSeen + ')');
+                        console.log('[ACTIVE] ' + row.username + ' visited '+ timeSince(row.lastSeen) + ' ago');
                     }
                 }
             });
+
         }
+
+        if (idleDJs.length > 0) {
+            var idleDJsList = idleDJs.join(', ');
+            bot.chat('Currently idle: ' + idleDJsList);
+        }
+
     }
 };
