0b62163cc34680857d27c7c04e2d9001de03c27f
mburke <michaelvburke@gmail.com>
2014-03-20 00:01:36 -0500

    * Initial test of AFK poll for mods

---
+++ commands/afk.js
@@ -0,0 +1,24 @@
+exports.names = ['.afk'];
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data) {
+    if (room.staff[data.fromID] > 1 && config.activeDJTimeoutMins > 0) {
+        maxIdleTime = config.activeDJTimeoutMins * 60;
+
+        // Start at slot 1 (skip the current DJ)
+        for (i = 1; i < room.djs.length; i++) {
+            dj = room.djs[i].user;
+            db.get("SELECT strftime('%s', 'now')-strftime('%s', lastSeen) AS 'secondsSinceLastVisit', lastSeen FROM USERS WHERE userid = ?", [dj.id] , function (error, row) {
+                if (row != null) {
+                    if(row.secondsSinceLastVisit >= maxIdleTime) {
+                        console.log('[IDLE] ' + data.username + ' visited '+ row.secondsSinceLastVisit + ' seconds ago (' + row.lastSeen + ')');
+                    }
+                    else {
+                        console.log('[ACTIVE] ' + data.username + ' visited '+ row.secondsSinceLastVisit + ' seconds ago (' + row.lastSeen + ')');
+                    }
+                }
+            });
+        }
+    }
+};
