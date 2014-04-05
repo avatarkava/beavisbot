6014919732469bbecd67f5f95cd9c85632ab3cfb
mburke <michaelvburke@gmail.com>
2014-04-04 22:17:25 -0500

    * Yay for consistency in capitalization

--- commands/bettyparty.js
+++ commands/bettyparty.js
@@ -3,5 +3,10 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function (data) {
-    bot.chat('http://i.imgur.com/qVnb7Io.gif');
+    var strings = [
+        'http://31.media.tumblr.com/tumblr_m4k3xogOGu1rn95k2o1_250.gif',
+        'http://belieber.files.wordpress.com/2010/10/2iurhw3.gif'
+    ];
+    var randomIndex = Math.floor(Math.random() * strings.length);
+    bot.chat(strings[randomIndex]);
 };
