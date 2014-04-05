79c3ae9f0467eadc877d3bfe1efd84bbd315e6d5
mburke <michaelvburke@gmail.com>
2014-04-04 21:02:23 -0500

    * Betty Party + New Opinion Graphic

---
+++ commands/bettyparty.js
@@ -0,0 +1,7 @@
+exports.names = ['.bettyparty'];
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function (data) {
+    bot.chat('http://i.imgur.com/qVnb7Io.gif');
+};
--- commands/opinion.js
+++ commands/opinion.js
@@ -1,7 +1,12 @@
-exports.names = ['.opinion'];
+exports.names = ['.opinion', '.patrice'];
 exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function (data) {
-    bot.chat('http://i.imgur.com/SH6qTAI.png');
+    var strings = [
+        'http://i.imgur.com/CifeChq.gif',
+        'http://i.imgur.com/SH6qTAI.png'
+    ];
+    var randomIndex = Math.floor(Math.random() * strings.length);
+    bot.chat(strings[randomIndex]);
 };
