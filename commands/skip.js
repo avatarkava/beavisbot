eda50bb15f0fd7e2a198bed6d29a1b03cc84185e
mburke <michaelvburke@gmail.com>
2014-04-05 13:35:08 -0500

    * Update lastplayed formatting * Give usage recommendations for updatetitle/updateauthor * Allow user to .skip their own song.

--- commands/skip.js
+++ commands/skip.js
@@ -3,7 +3,7 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
-    if (room.staff[data.fromID] > 1) {
+    if (room.staff[data.fromID] > 1 || data.fromID == room.djs[0].user.id) {
         bot.moderateForceSkip();
     }
 };
