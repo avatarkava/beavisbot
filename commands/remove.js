3c61fada4fef3c4ae4c9199a8b217bef42d76103
mburke <michaelvburke@gmail.com>
2014-04-24 19:59:23 -0500

    Trim spaces

--- commands/remove.js
+++ commands/remove.js
@@ -5,7 +5,7 @@ exports.matchStart = true;
 exports.handler = function(data) {
     if (room.staff[data.fromID] > 1) {
         var input = data.message.split(' ');
-        var username = _.rest(input, 1).join(' ');
+        var username = _.rest(input, 1).join(' ').trim();
         if (username) {
             db.get('SELECT * FROM USERS LEFT JOIN DISCIPLINE USING(userid) WHERE username = ?', [username.substring(1)], function (error, row) {
                 bot.moderateRemoveDJ(row.userid);
