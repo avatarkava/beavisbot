0bbc3d67387b866992d5ba13704f4d5617ef476c
mburke <michaelvburke@gmail.com>
2014-05-05 21:35:35 -0500

    * Attempt at bot policing of max song length when no mods active

--- sparkle.js
+++ sparkle.js
@@ -178,32 +178,45 @@ function runBot(error, auth) {
                 correctMetadata();
             }
 
-            if (config.activeDJTimeoutMins > 0) {
-                var maxIdleTime = config.activeDJTimeoutMins * 60;
-                var idleDJs = [];
-                var z = 0;
+            var maxIdleTime = config.activeDJTimeoutMins * 60;
+            var roomHasActiveMods = false;
+            var idleDJs = [];
+            var z = 0;
 
-                waitlist = bot.getDJs().splice(1);
-                waitlist.forEach(function(dj) {
-                    db.get("SELECT strftime('%s', 'now')-strftime('%s', lastActive) AS 'secondsSinceLastActive', lastActive, username FROM USERS WHERE userid = ?", [dj.id] , function (error, row) {
-                        z++;
-                        if (row != null) {
-                            if(row.secondsSinceLastActive >= maxIdleTime) {
-                                bot.log('[IDLE] ' + z + '. ' + row.username + ' last active '+ moment.utc(row.lastActive).fromNow());
-                                idleDJs.push(row.username);
-                            }
-                            else {
-                                bot.log('[ACTIVE] ' + z + '. ' + row.username + ' last active '+ moment.utc(row.lastActive).fromNow());
-                            }
+            waitlist = bot.getDJs().splice(1);
+            waitlist.forEach(function(dj) {
+                db.get("SELECT strftime('%s', 'now')-strftime('%s', lastActive) AS 'secondsSinceLastActive', lastActive, username FROM USERS WHERE userid = ?", [dj.id] , function (error, row) {
+                    z++;
+                    if (row != null) {
+
+                        if(row.secondsSinceLastActive >= maxIdleTime) {
+                            bot.log('[IDLE] ' + z + '. ' + row.username + ' last active '+ moment.utc(row.lastActive).fromNow());
+                            idleDJs.push(row.username);
                         }
+                        else {
+                            if(dj.permission > 1) {
+                                roomHasActiveMods = true;
+                            }
+                            bot.log('[ACTIVE] ' + z + '. ' + row.username + ' last active '+ moment.utc(row.lastActive).fromNow());
+                        }
+                    }
 
-                        if (z == waitlist.length && idleDJs.length > 0) {
+                    if (z == waitlist.length) {
+
+                        if(idleDJs.length > 0) {
                             var idleDJsList = idleDJs.join(' @');
                             bot.sendChat('@' + idleDJsList + ' ' + config.responses.activeDJReminder);
                         }
-                    });
+
+                        // Only police this if there aren't any mods around
+                        if(!roomHasActiveMods && config.maxSongLengthSecs > 0 && data.media.duration > config.maxSongLengthSecs) {
+                            bot.log('[SKIP] Skipped ' + data.dj.user.username + ' spinning a song of ' + data.media.duration + ' seconds');
+                            bot.sendChat('Sorry @' + data.dj.user.username + ', this song is over our maximum room length of ' + config.maxSongLengthSecs + ' seconds.');
+                            bot.moderateForceSkip();
+                        }
+                    }
                 });
-            }
+            });
         }
         lastRpcMessage = new Date();
     });
