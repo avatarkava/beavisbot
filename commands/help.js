7ccd5cd9b0df73aedb720461cfa972b1cabf01f7
mburke <michaelvburke@gmail.com>
2014-06-16 19:56:54 -0500

    Add .support

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
-exports.names = ['.help'];
+exports.names = ['.help', '.support'];
 exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function (data) {
-    bot.sendChat('Need help? Ask a mod! No mods around? http://plug.dj/support');
+    bot.sendChat('Need help? Ask a mod! No mods around? Contact a Brand Ambassador: http://plug.dj/support - Hours: http://blog.plug.dj/brand-ambassadors/');
 };
