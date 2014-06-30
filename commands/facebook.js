f11c28575cc83311ece6b9420c911b598befce67
mburke <michaelvburke@gmail.com>
2014-06-30 08:35:31 -0500

    Add links to facebook group

---
+++ commands/facebook.js
@@ -0,0 +1,7 @@
+exports.names = ['.facebook', '.fb'];
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function (data) {
+    bot.sendChat('Join our Facebook group! https://www.facebook.com/groups/iheart80sand90s/');
+};
