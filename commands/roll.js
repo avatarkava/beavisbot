f53259f106703a1c9c724805a4884e61987d0cd9
mburke <michaelvburke@gmail.com>
2014-08-13 02:07:47 -0500

    Nerd dice

--- commands/roll.js
+++ commands/roll.js
@@ -7,12 +7,17 @@ exports.handler = function (data) {
     var maxValue = 6;
 
     var input = _.rest(data.message.split(' '), 1).join(' ').trim();
+
     if (input.length > 0) {
-        maxValue = parseInt(input.replace(/[^0-9]/g, ""));
+        maxValue = parseInt(input.replace(/\D/g, ""));
     }
 
-    if (maxValue > 0) {
+    if (maxValue > 0 && maxValue < 99999) {
         var roll = _.random(1, maxValue);
         bot.sendChat(data.from + ', you rolled a ' + roll + '!');
     }
+    else {
+        bot.sendChat('http://www.clownshoes.net/clownshoes.gif');
+    }
+
 };
