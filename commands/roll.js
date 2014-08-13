9ed037235a04c321b67a7bcb8f48deedfa26789d
mburke <michaelvburke@gmail.com>
2014-08-13 01:49:22 -0500

    Nerd dice

--- commands/roll.js
+++ commands/roll.js
@@ -3,6 +3,16 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function (data) {
-    var roll = _.random(1, 6);
-    bot.sendChat(data.from + ', you rolled a ' + roll + '!');
+
+    var maxValue = 6;
+
+    var input = _.rest(data.message.split(' '), 1).join(' ').trim();
+    if (input.length > 0) {
+        maxValue = parseInt(input.replace(/[^0-9]/g, ""));
+    }
+
+    if (maxValue > 0) {
+        var roll = _.random(1, maxValue);
+        bot.sendChat(data.from + ', you rolled a ' + roll + '!');
+    }
 };
