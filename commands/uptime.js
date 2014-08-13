a139ae100f80d8398e73877197bc42829bde458e
mburke <michaelvburke@gmail.com>
2014-08-13 01:00:32 -0500

    * Uptime update

--- commands/uptime.js
+++ commands/uptime.js
@@ -3,5 +3,5 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function (data) {
-    bot.sendChat(timeSince(uptime.getTime()) + ' (' + uptime.getTime() + ')');
+    bot.sendChat('up since ' + moment.utc(uptime.getTime(), 'YYYY-MM-DD HH:mm:ss').calendar() + ' UTC (' + timeSince(uptime.getTime()) + ' )');
 };
