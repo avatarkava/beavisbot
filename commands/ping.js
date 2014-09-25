f26328f71cfb37fdca5905e2ae52582d0d1bb64d
mburke <michaelvburke@gmail.com>
2014-09-24 20:40:56 -0500

    * Update .gitignore * Make most of web 'sample' stuff instead of forcing it into the repo

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-exports.names = ['.ping'];
-exports.hidden = true;
-exports.enabled = false;
-exports.matchStart = false;
-exports.handler = function (data) {
-    bot.sendChat('Still here, ' + data.from.username + '!');
-};
