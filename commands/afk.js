6281b30d42a831ec718617e8d67624c2a556b6f3
mburke <michaelvburke@gmail.com>
2014-10-23 11:40:45 -0500

    Add ability for mod to reset idle timer for a user (chat splits are awsm)

--- commands/afk.js
+++ commands/afk.js
@@ -3,37 +3,50 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function (data) {
+
+    var input = data.message.split(' ');
+
     if (data.from.role > 1 && config.activeDJTimeoutMins > 0) {
 
-        var maxIdleTime = config.activeDJTimeoutMins * 60;
-        idleDJs = [];
-        z = 0;
+        if (input.length >= 2) {
+            username = _.initial(username, 1).join(' ').trim();
+            var usernameFormatted = S(username).chompLeft('@').s;
 
-        waitlist = bot.getDJs();
-        logger.warning(JSON.stringify(waitlist, null, 2));
+            User.update({last_active: new Date(), last_seen: new Date()}, {where: {username: usernameFormatted}});
+            bot.sendChat("/me reset the idle timer for " + usernameFormatted);
 
-        Promise.map(bot.getWaitList(), function (dj) {
-            return User.find({where: {id: dj.id}}).on('success', function (dbUser) {
-                var position = bot.getWaitListPosition(dj.id);
-                if (dbUser !== null) {
-                    if (secondsSince(dbUser.last_active) >= maxIdleTime && moment.utc().isAfter(moment.utc(startupTimestamp).add(config.activeDJTimeoutMins, 'minutes'))) {
-                        logger.warning('[IDLE]', position + '. ' + dbUser.username + ' last active ' + timeSince(dbUser.last_active));
-                        idleDJs.push(dbUser.username);
+        }
+        else {
+            var maxIdleTime = config.activeDJTimeoutMins * 60;
+            idleDJs = [];
+            z = 0;
 
+            waitlist = bot.getDJs();
+            logger.warning(JSON.stringify(waitlist, null, 2));
+
+            Promise.map(bot.getWaitList(), function (dj) {
+                return User.find({where: {id: dj.id}}).on('success', function (dbUser) {
+                    var position = bot.getWaitListPosition(dj.id);
+                    if (dbUser !== null) {
+                        if (secondsSince(dbUser.last_active) >= maxIdleTime && moment.utc().isAfter(moment.utc(startupTimestamp).add(config.activeDJTimeoutMins, 'minutes'))) {
+                            logger.warning('[IDLE]', position + '. ' + dbUser.username + ' last active ' + timeSince(dbUser.last_active));
+                            idleDJs.push(dbUser.username);
+
+                        }
+                        else {
+                            logger.info('[ACTIVE]', position + '. ' + dbUser.username + ' last active ' + timeSince(dbUser.last_active));
+                        }
                     }
-                    else {
-                        logger.info('[ACTIVE]', position + '. ' + dbUser.username + ' last active ' + timeSince(dbUser.last_active));
-                    }
+                });
+            }).then(function () {
+                if (idleDJs.length > 0) {
+                    var idleDJsList = idleDJs.join(' • ');
+                    bot.sendChat("Currently idle: " + idleDJsList);
+                }
+                else {
+                    bot.sendChat("Everyone's currently active! :thumbsup:");
                 }
             });
-        }).then(function () {
-            if (idleDJs.length > 0) {
-                var idleDJsList = idleDJs.join(' • ');
-                bot.sendChat("Currently idle: " + idleDJsList);
-            }
-            else {
-                bot.sendChat("Everyone's currently active! :thumbsup:");
-            }
-        });
+        }
     }
 }
