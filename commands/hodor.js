bc02f1d024aacb76ce0f334bff34df7f6c0beeef
takahan <la9689@gmail.com>
2015-04-15 23:11:38 -0300

    hodor.js
    no use

--- commands/hodor.js
+++
@@ -1,7 +0,0 @@
-exports.names = ['.hodor', 'hodor', 'hodor?'];
-exports.hidden = true;
-exports.enabled = true;
-exports.matchStart = false;
-exports.handler = function (data) {
-    bot.sendChat('Hodor!');
-};
