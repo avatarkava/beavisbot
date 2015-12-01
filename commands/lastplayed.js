67942f534ca25c1bd610bb9e96d694b3d37d5e03
avatarkava <mburke@phpmonkeys.com>
2015-11-30 21:38:08 -0600

    * Fix lastplayed orderby in sequelize

--- commands/lastplayed.js
+++ commands/lastplayed.js
@@ -14,11 +14,12 @@ exports.handler = function (data) {
 
     models.Play.findOne({
         include: [{model: models.Song, where: {$or: [{site_id: songid}, {host_id: songid}]}}, models.User],
-        order: [Play, 'created_at', 'DESC']
+        order: [['created_at', 'DESC']]
     }).then(function (row) {
-        if (row === null) {
+        if (!row) {
             bot.sendChat('This is the first time I have seen this song played in this room!');
         } else {
+            console.log('This other crap');
             message = row.Song.name + ' • last played ' + timeSince(row.created_at) + ' by ' + row.User.username
                 + ' • ' + row.listeners + ' listeners • ' + (row.positive - row.negative) + ' dubs';
             bot.sendChat(message);
