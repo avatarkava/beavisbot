1d228d9d06be290be1c6926b6111dd0cd53aaad6
avatarkava <mburke@phpmonkeys.com>
2015-12-17 20:51:27 -0600

    Update schema

--- install/tables.sql
+++ install/tables.sql
@@ -1,157 +1,141 @@
 DROP TABLE IF EXISTS `event_responses`;
 CREATE TABLE `event_responses` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`event_type`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`trigger`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci ,
-`response`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`is_active`  tinyint(1) NULL DEFAULT 0 ,
-`created_at`  datetime NOT NULL ,
-`updated_at`  datetime NOT NULL ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `event_type` varchar(255) NOT NULL,
+  `trigger` varchar(255) DEFAULT NULL,
+  `response` varchar(255) NOT NULL,
+  `is_active` tinyint(1) DEFAULT '1',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `games`;
 CREATE TABLE `games` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`type`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`user_id`  int(10) UNSIGNED NOT NULL ,
-`mod_user_id`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`details`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL ,
-`result`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL ,
-`participants`  int(10) UNSIGNED NULL DEFAULT 0 ,
-`created_at`  datetime NOT NULL ,
-`updated_at`  datetime NOT NULL ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `details` text,
+  `result` text,
+  `participants` int(10) unsigned DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `mod_user_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `mod_user_id` (`mod_user_id`),
+  CONSTRAINT `games_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `games_ibfk_2` FOREIGN KEY (`mod_user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `karmas`;
 CREATE TABLE `karmas` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`type`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`user_id`  int(10) UNSIGNED NOT NULL ,
-`mod_user_id`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`details`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL ,
-`created_at`  datetime NOT NULL ,
-`updated_at`  datetime NOT NULL ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `details` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `mod_user_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `mod_user_id` (`mod_user_id`),
+  CONSTRAINT `karmas_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `karmas_ibfk_2` FOREIGN KEY (`mod_user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `plays`;
 CREATE TABLE `plays` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`user_id`  int(10) UNSIGNED NOT NULL ,
-`song_id`  int(11) UNSIGNED NOT NULL ,
-`positive`  int(10) UNSIGNED NULL DEFAULT 0 ,
-`negative`  int(10) UNSIGNED NULL DEFAULT 0 ,
-`grabs`  int(10) UNSIGNED NULL DEFAULT 0 ,
-`listeners`  int(10) UNSIGNED NULL DEFAULT 0 ,
-`skipped`  int(10) UNSIGNED NULL DEFAULT 0 ,
-`created_at`  datetime NOT NULL ,
-`updated_at`  datetime NOT NULL ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `site_id` varchar(255) NOT NULL,
+  `positive` int(10) unsigned DEFAULT '0',
+  `negative` int(10) unsigned DEFAULT '0',
+  `grabs` int(10) unsigned DEFAULT '0',
+  `listeners` int(10) unsigned DEFAULT '0',
+  `skipped` int(10) unsigned DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `song_id` int(10) unsigned DEFAULT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `song_id` (`song_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `plays_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `plays_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `room_events`;
 CREATE TABLE `room_events` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`type`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`title`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`slug`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`details`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL ,
-`starts_at`  datetime NULL DEFAULT NULL ,
-`ends_at`  datetime NULL DEFAULT NULL ,
-`created_at`  datetime NOT NULL ,
-`updated_at`  datetime NOT NULL ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
-
-DROP TABLE IF EXISTS `songs`;
-CREATE TABLE `songs` (
-`id`  int(10) UNSIGNED NOT NULL DEFAULT 0 ,
-`author`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`title`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`slug`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`release_date`  datetime NULL DEFAULT NULL ,
-`tags`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`format`  int(10) UNSIGNED NULL DEFAULT 1 ,
-`cid`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`plug_id` int(10) UNSIGNED NULL DEFAULT NULL ,
-`duration`  int(10) UNSIGNED NULL DEFAULT NULL ,
-`image`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`is_banned`  tinyint(1) NULL DEFAULT 0 ,
-`created_at`  datetime NOT NULL ,
-`updated_at`  datetime NOT NULL ,
-PRIMARY KEY (`id`) ,
-UNIQUE KEY `format_cid` (`format`,`cid`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `details` text,
+  `starts_at` datetime DEFAULT NULL,
+  `ends_at` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `mod_user_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `mod_user_id` (`mod_user_id`),
+  CONSTRAINT `room_events_ibfk_1` FOREIGN KEY (`mod_user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `song_responses`;
 CREATE TABLE `song_responses` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`media_type`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`trigger`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`response`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`rate`  int(11) NULL DEFAULT '0' ,
-`is_active`  tinyint(1) NULL DEFAULT 0 ,
-`created_at`  datetime NOT NULL ,
-`updated_at`  datetime NOT NULL ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `media_type` varchar(255) DEFAULT NULL,
+  `trigger` varchar(255) NOT NULL,
+  `response` varchar(255) DEFAULT NULL,
+  `rate` int(11) DEFAULT '0',
+  `is_active` tinyint(1) DEFAULT '1',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-;
+DROP TABLE IF EXISTS `songs`;
+CREATE TABLE `songs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `site_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `release_date` date DEFAULT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  `host` varchar(255) NOT NULL DEFAULT 'youtube',
+  `host_id` varchar(255) NOT NULL,
+  `duration` int(10) unsigned DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `is_banned` tinyint(1) DEFAULT '0',
+  `banned_reason` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `site_id` (`site_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=228 DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-`id`  int(10) UNSIGNED NOT NULL DEFAULT 0 ,
-`username`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`slug`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`language`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT 'en' ,
-`avatar_id`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`badge`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`blurb`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL ,
-`global_role`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`mod_user_id`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`role`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`level`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`experience_points`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`plug_points`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`custom_points`  int(11) UNSIGNED NULL DEFAULT 0 ,
-`joined`  datetime NULL DEFAULT NULL ,
-`birthday`  datetime NULL DEFAULT NULL ,
-`waitlist_position`  int(11) NULL DEFAULT '-1' ,
-`last_seen`  datetime NULL DEFAULT NULL ,
-`last_active`  datetime NULL DEFAULT NULL ,
-`created_at`  datetime NOT NULL ,
-`updated_at`  datetime NOT NULL ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `site_id` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `locale` varchar(255) DEFAULT 'en_US',
+  `avatar` varchar(255) DEFAULT NULL,
+  `bio` text,
+  `role` varchar(255) DEFAULT NULL,
+  `site_points` int(10) unsigned DEFAULT '0',
+  `custom_points` int(10) unsigned DEFAULT '0',
+  `joined` datetime DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `queue_position` int(11) DEFAULT '-1',
+  `last_seen` datetime DEFAULT NULL,
+  `last_active` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `site_id` (`site_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
