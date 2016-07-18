7f3b3f2f11d41269e8c5964d952cf283febd09d9
avatarkava <mburke@phpmonkeys.com>
2016-07-17 21:38:16 -0500

    Try to fix spaces when gifting (man spaces in usernames are the worst)

--- commands/gift.js
+++ commands/gift.js
@@ -5,23 +5,20 @@ exports.matchStart = true;
 exports.handler = function (data) {
 
     var input = data.message.split(' ');
-    var username = input[1];
+    var params = _.rest(input, 1);
+    var username = _.initial(params);
+    var amount = parseInt(_.last(params));
 
-    if (!username) {
+    if (!amount || isNaN(amount) || amount < 1 || !username) {
         bot.sendChat('/me usage: .gift @username 50 (gifts 50 points)');
         return;
     }
 
     var usernameFormatted = S(username).chompLeft('@').s;
-    var amount = parseInt(input[2]);
     var user = findUserInList(bot.getUsers(), usernameFormatted);
 
     if (!user) {
         bot.sendChat('/me user ' + username + ' was not found.');
-    }
-
-    if (!amount || isNaN(amount) || amount < 1) {
-        amount = 1;
     }
 
     getDbUserFromSiteUser(data.from, function (row) {
@@ -29,7 +26,7 @@ exports.handler = function (data) {
             bot.sendChat('You do not have any ' + config.customPointName + ' to give, @' + data.from.username + '.');
             return;
         } else if (row.custom_points < amount) {
-            bot.sendChat('You only have ' + row.custom_points + ' ' +  config.customPointName + ' to give, @' +
+            bot.sendChat('You only have ' + row.custom_points + ' ' + config.customPointName + ' to give, @' +
                 data.from.username + '.');
             return;
         }
