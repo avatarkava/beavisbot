2f335034bf684e079a533d0e3805d8efe9df4b9a
avatarkava <mburke@phpmonkeys.com>
2016-11-02 19:39:16 -0500

    Add in handling for a user being unmuted

--- events/modmute.js
+++ events/modmute.js
@@ -15,15 +15,19 @@ module.exports = function (bot) {
             case 'l':
                 duration = '45';
                 break;
+            default:
+                // maybe this is an unmute?
+                break;
         }
 
         getDbUserFromUsername(data.t, function (dbUser) {
             var message;
-            if (dbUser == null) {
-                message = '[MUTE] ' + data.t + ' (ID: ' + data.i + ') was muted for ' + duration + ' minutes by ' + data.m;
-                message = '[MUTE] ' + data.t + ' (ID: ' + data.i + ') was muted for ' + duration + ' minutes by ' + data.m;
+            if (duration == 'unknown') {
+                message = '[UNMUTE] ' + data.t + ' (ID:' + data.i + ') was unmuted by ' + data.m;
+            } else if (dbUser == null) {
+                message = '[MUTE] ' + data.t + ' (ID:' + data.i + ') was muted for ' + duration + ' minutes by ' + data.m;
             } else {
-                message = '[BAN] ' + data.t + ' (ID: ' + data.i + ', LVL: ' + dbUser.site_points + ') was muted for ' + duration + ' minutes by ' + data.m;
+                message = '[MUTE] ' + data.t + ' (ID:' + data.i + ', LVL:' + dbUser.site_points + ') was muted for ' + duration + ' minutes by ' + data.m;
             }
             console.log(message);
             sendToSlack(message);
