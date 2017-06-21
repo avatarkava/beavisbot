cea00cb4ca9654103b2d3fea998712d70876ab19
Mike Burke <michaelvburke@gmail.com>
2017-06-20 22:03:06 -0500

    Kill 'em with kindness

--- events/djlistupdate.js
+++ events/djlistupdate.js
@@ -11,13 +11,13 @@ module.exports = function (bot) {
 
                 if (listUser['role'] == 0 && listUser['gRole'] == 0 && listUser['level'] < config.queue.djMinLevel) {
                     logMessage = '[REMOVE] Removed ' + listUser.username + ' joining the wait list below minimum level ' + config.queue.djMinLevel;
-                    message = 'Sorry @' + listUser.username + ', this community does not allow joining the wait list until you reach level ' + config.queue.djMinLevel + '.';
+                    message = 'Sorry @' + listUser.username + ', this community does not allow joining the wait list until you reach level ' + config.queue.djMinLevel + '. Enjoy listening until then!';
                     remove = true;
                 } else {
                     if (secondsUntil(row.dj_timeout_until) > 0) {
                         expiry = moment.utc(row.dj_timeout_until).format('MMM DD, YYYY') + ' at ' + moment.utc(row.dj_timeout_until).format('h:mmA') + ' UTC';
                         logMessage = '[REMOVE] Removed ' + listUser.username + ' joining the wait list while timed out until ' + expiry;
-                        message = 'Sorry @' + listUser.username + ', you are prohibited from entering the wait list until ' + expiry + ' ' + timeUntil(row.dj_timeout_until);
+                        message = 'Sorry @' + listUser.username + ', you are prohibited from entering the wait list until ' + expiry + ' ' + timeUntil(row.dj_timeout_until) + '. Enjoy listening until then!';
                         remove = true;
                     }
                 }
