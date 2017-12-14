c391a2e801de006bb7c79e24433fcd661530caab
Mike Burke <michaelvburke@gmail.com>
2017-12-14 13:13:03 -0600

    Enable waitlist ban logging to slack and DJ waitlist adjustments to console

--- events/modWaitlistBan.js
+++ events/modWaitlistBan.js
@@ -1,7 +1,42 @@
 module.exports = function (bot) {
 
     bot.on(PlugAPI.events.MODERATE_WLBAN, function (data) {
-        console.log('[EVENT] modWaitlistBan ', JSON.stringify(data, null, 2));
+
+        if (config.verboseLogging) {
+            console.log('[EVENT] modWaitlistBan ', JSON.stringify(data, null, 2));
+        }
+
+        var duration = 'unknown';
+        switch (data.duration) {
+            case 'Short':
+                duration = '15 min';
+                break;
+            case 'Medium':
+                duration = '30 min';
+                break;
+            case 'Long':
+                duration = '45 min';
+                break;
+            case 'Forever':
+                duration = 'forever';
+                break;
+            default:
+                // maybe this is an unmute?
+                break;
+        }
+
+        getDbUserFromUsername(data.user.username, function (dbUser) {
+            var message;
+            if (duration == 'unknown') {
+                message = '[UNMUTE] ' + data.user.username + ' (ID: ' + data.user.id + ') was unbanned from the waitlist by ' + data.moderator.username;
+            } else if (dbUser == null) {
+                message = '[MUTE] ' + data.user.username + ' (ID: ' + data.user.id + ') was banned from the waitlist for ' + duration + ' by ' + data.moderator.username;
+            } else {
+                message = '[MUTE] ' + data.user.username + ' (ID: ' + data.user.id + ', LVL: ' + dbUser.site_points + ') was banned from the waitlist for ' + duration + ' by ' + data.moderator.username;
+            }
+            console.log(message);
+            sendToSlack(message);
+        });
     });
-    
+
 };
