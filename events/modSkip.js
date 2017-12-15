777c04872f848eb3ec58de89fd1d144ed5803af7
Mike Burke <michaelvburke@gmail.com>
2017-12-15 01:06:49 -0600

    Update skip data

--- events/modSkip.js
+++ events/modSkip.js
@@ -6,16 +6,21 @@ module.exports = function (bot) {
         }
 
         // Send this if the skip didn't come via [SKIP]
-        if (data.mi != botUser.db.site_id) {
-            message = '[SKIP] ' + data.m + ' skipped a song.';
-            console.log(message + ' ' + JSON.stringify(data, null, 2));
+        if (data.user.id != botUser.db.site_id) {
+            message = '[SKIP] ' + data.user.username + ' skipped a song.';
+            console.log(message);
             sendToSlack(message);
         }
 
         // Data from last song played
         var skippedSong = bot.lastPlay;
         message = '[SKIP] Skipped song: ' + skippedSong.media.name + ' (https://www.youtube.com/watch?v=' + skippedSong.media.cid + ') played by ' + skippedSong.dj.username + ' (ID: ' + skippedSong.dj.id + ')';
-        console.log('[SKIP] ' + JSON.stringify(skippedSong, null, 2));
+
+        if (config.verboseLogging) {
+            console.log('[SKIP] ' + message, JSON.stringify(skippedSong, null, 2));
+        } else {
+            console.log('[SKIP] ' + message);
+        }
         sendToSlack(message);
     });
 };
